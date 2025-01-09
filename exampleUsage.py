89edb8c52c1e22ee9d8a5529434bdaf1de03aef1
Bart Wolleswinkel <b.wolleswinkel@tudelft.nl>
2025-01-09 20:38:51 +0100

    first commit

---
+++ exampleUsage.py
@@ -0,0 +1,6 @@
+"""
+This script explains how the multi-objective control toolbox can interact with the TWAIN environment and returns the result to the users.
+"""
+
+# Import packages
+import pandas as pd
---
+++ readme.md
@@ -0,0 +1,9 @@
+Work in progress (WIP)
+
+## Multi-objective control toolbox
+
+This toolbox ...
+
+## Licensing
+
+No licensing, which means this project is currently not in the public domain.
