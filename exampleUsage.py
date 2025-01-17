90a2e101c8cca2dd60d9a4fc37925d89e10e2d9b
Bart Wolleswinkel <b.wolleswinkel@tudelft.nl>
2025-01-17 13:01:29 +0100

    Added some dummy things to exampleUsage.py

--- exampleUsage.py
+++ exampleUsage.py
@@ -4,3 +4,11 @@ This script explains how the multi-objective control toolbox can interact with t
 
 # Import packages
 import pandas as pd
+import numpy as np
+import twain.moct as moct
+
+# Give your credentials
+USERNAME, PASSWORD = 'test-user-001', 'test-password-001'
+
+# Launch the GUI
+moct.open()
