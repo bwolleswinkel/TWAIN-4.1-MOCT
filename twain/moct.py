f68009af6f948ea634848f7931f43ddee584a6da
Bart Wolleswinkel <b.wolleswinkel@tudelft.nl>
2025-01-22 18:14:47 +0100

    Added lot of example files, renamed some stuff

---
+++ twain/moct.py
@@ -0,0 +1,20 @@
+"""
+This is the high-level module for the multi-objective control toolbox (MOCT) of the TWAIN environment.
+"""
+
+# Import packages
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+
+def open(connect: dict = None, dir_local: Path | str = None):
+    """
+    This function launches the GUI for the multi-objective control toolbox (MOCT) of the TWAIN environment.
+
+    Parameters
+    ----------
+    connect : dict, optional
+        The connection details to the TWAIN environment, by default None
+    """
+    pass
