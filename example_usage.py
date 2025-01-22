97cc3f2b61911b2393c1f168ba35883cba2c27f3
Bart Wolleswinkel <b.wolleswinkel@tudelft.nl>
2025-01-22 18:55:30 +0100

    Added some mock optimization function

--- example_usage.py
+++ example_usage.py
@@ -2,13 +2,14 @@
 This script explains how the multi-objective control toolbox can interact with the TWAIN environment and returns the result to the users.
 """
 
-# Import packages
 import pandas as pd
 import numpy as np
+
 import twain.moct as moct
 
-# Give your credentials
-USERNAME, PASSWORD = 'test-user-001', 'test-password-001'
+# Set the parameters
+wind_farm_layout = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+wind_direction = 270  # In degrees
 
-# Launch the GUI
-moct.open()
+# Solve for optimal yaw angles
+yaw_angles_opt = moct.yaw_angles(wind_farm_layout, wind_direction)
--- twain/moct.py
+++ twain/moct.py
@@ -2,10 +2,27 @@
 This is the high-level module for the multi-objective control toolbox (MOCT) of the TWAIN environment.
 """
 
-# Import packages
+from typing import TypeVar, TypeAlias
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
-from pathlib import Path
+import numpy.typing as npt
+
+# ------ TYPE ALIASES ------
+
+T = TypeVar('T', int, float, complex, bool)
+NPArray: TypeAlias = npt.NDArray[np.dtype[T]]
+
+# ------ CLASSES ------
+
+
+def Optimizer():
+    def __init__(self):
+        pass
+
+
+# ------ FUNCTIONS ------
 
 
 def open(connect: dict = None, dir_local: Path | str = None):
@@ -18,3 +35,28 @@ def open(connect: dict = None, dir_local: Path | str = None):
         The connection details to the TWAIN environment, by default None
     """
     pass
+
+
+# FIXME: What we actually want is a separate subdirectory with a module called 'optimize.py' that contains the following function, such that we can call 'moct.optimize.yaw_angles' in the example_usage.py script.
+def yaw_angles(wind_farm_layout: NPArray[float], wind_direction: float) -> NPArray[float]:
+    """
+    Given a wind farm layout and a wind direction, this function solves for the optimal yaw angles of the wind turbines.
+
+    Parameters
+    ----------
+    wind_farm_layout :
+        The layout of the wind farm, where each row is a wind turbine and the columns are the x and y coordinates, respectively.
+    wind_direction :
+        The wind direction for which to optimize the yaw angles.
+
+    Returns
+    -------
+    optimal_yaw_angles :
+        The optimal yaw angles of the wind turbines.
+    """
+    # FIXME: Perform some check on the input
+    n_wt = wind_farm_layout.shape[0]
+    optimal_yaw_angles = np.random.rand(n_wt)
+    return optimal_yaw_angles
+
+        
